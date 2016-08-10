10c39e79707ba80361eb5ad61f4c1df621a34882
Administrator <Administrator@WIN-4H0NFDDG8NO>
2016-08-11 01:32:04 +0900

    gagz

--- testgit/src/main_layout/bean/layout.java
+++ testgit/src/main_layout/bean/layout.java
@@ -1,5 +1,19 @@
 package main_layout.bean;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.ibatis.SqlMapClientTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class layout {
+	
+	@Autowired
+	private SqlMapClientTemplate sqlMap;
+	
+	@RequestMapping("popup.now")
+	public String main(){
+		return "/main_layout/mainLayout.jsp";
+	}
 
 }
